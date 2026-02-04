71e1c445bc13df2384f55475ee72931a3b92ec99
CASMulder2003 <ucfncmu@ucl.ac.uk>
2026-02-04 14:02:53 +0000

    updated vite config and package.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -11,5 +12,5 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  base: '/casa0028-trial-site-photography/',
+  base: '/casa0028-trial-site-photography/'
 })
